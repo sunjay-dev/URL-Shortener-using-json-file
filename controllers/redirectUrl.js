d0c416465f6a60755fab88d6c2164a9ccc68f993
Sunjay Kumar <111847625+sunjay-dev@users.noreply.github.com>
2024-10-20 22:51:00 +0500

    Updated path of 404 page

--- controllers/redirectUrl.js
+++ controllers/redirectUrl.js
@@ -48,7 +48,7 @@ function redirectUrl (req,res){
         return res.status(500).json({error: "Internal server error"});
     }
     if (!found) {
-        res.status(404).send("404 Not Found");
+        res.status(404).sendFile(path.join(__dirname,"../public", "404.html"));
     }
 }
 
